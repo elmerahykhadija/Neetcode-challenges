60aba4633bd8a1a8f645edc755a6cacbeaf2ca67
elmerahykhadija <elmerahykhadija700@gmail.com>
2026-09-19 16:35:54 +0100

    balanced tree

--- Trees/Diameter of Binary Tree.py
+++ Trees/Diameter of Binary Tree.py
@@ -5,3 +5,23 @@ The length of a path between two nodes in a binary tree is the number of edges b
 
 Given the root of a binary tree root, return the diameter of the tree.
 """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diametr=0
+        def hauteur(root):
+            nonlocal diametr
+            if root is None:
+                return 0
+            left=hauteur(root.left)
+            right=hauteur(root.right)
+            diametr=max(diametr,left+right)
+            return 1+max(left,right) 
+        hauteur(root)
+        return diametr
